aa840aef8fa597f03cdb6216fd652a3ece78673f
Rakshithvpatil <patilrakshith3@gmail.com>
2025-03-02 02:33:47 -0600

    Create CNN.py

---
+++ CNN.py
@@ -0,0 +1,27 @@
+def convolutional_neural_network(x):
+    weights = {'W_conv1':tf.Variable(tf.random_normal([5,5,5,1,32])),
+               'W_conv2':tf.Variable(tf.random_normal([5,5,5,32,64])),
+               'W_fc':tf.Variable(tf.random_normal([54080,1024])),
+               'out':tf.Variable(tf.random_normal([1024, n_classes]))}
+
+    biases = {'b_conv1':tf.Variable(tf.random_normal([32])),
+               'b_conv2':tf.Variable(tf.random_normal([64])),
+               'b_fc':tf.Variable(tf.random_normal([1024])),
+               'out':tf.Variable(tf.random_normal([n_classes]))}
+
+    x = tf.reshape(x, shape=[-1, IMG_PXL_SIZE, IMG_PXL_SIZE, HM_SLICES, 1])
+
+    conv1 = tf.nn.relu(conv3d(x, weights['W_conv1']) + biases['b_conv1'])
+    conv1 = maxpool3d(conv1)
+
+    conv2 = tf.nn.relu(conv3d(conv1, weights['W_conv2']) + biases['b_conv2'])
+    conv2 = maxpool3d(conv2)
+    
+
+    fc = tf.reshape(conv2,[-1, 54080])
+    fc = tf.nn.relu(tf.matmul(fc, weights['W_fc'])+biases['b_fc'])
+    fc = tf.nn.dropout(fc, keep_rate)
+
+    output = tf.matmul(fc, weights['out'])+biases['out']
+
+    return output
